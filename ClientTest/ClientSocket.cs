440247b43965eeb323367ee4ef5238ab46bb75bf
Mihail-Shir <68156603+Mihail-Shir@users.noreply.github.com>
2021-03-19 21:26:20 +0300

    ClientAdd
    add clienttest for a future app on Wpf/WinForms

---
+++ ClientTest/ClientSocket.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ServerTestApp
+{
+    class ClientSocket
+    {
+        static int port = 8005;
+        static string adress = "127.0.0.1";
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(adress), port);
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(ipPoint);
+                Console.WriteLine("Соединение установлено. Для отключение отправьте: 0");
+                while (true)
+                {
+                    byte[] data;
+                    Console.WriteLine("Введите сообщение:");
+                    string message = Console.ReadLine();
+                    if (message==""||message==null)
+                    {
+                        message = "Пусто";
+                    }
+                    else if (message=="0")
+                    {
+                        data = Encoding.Unicode.GetBytes(message);
+                        socket.Send(data);
+                        Console.WriteLine("Вы разорвали соединение!");
+                        break;
+                    }
+                    data = Encoding.Unicode.GetBytes(message);
+                    socket.Send(data);
+
+                    int bytes = 0;
+                    data = new byte[256];
+                    StringBuilder builder = new StringBuilder();
+
+                    do
+                    {
+                        bytes = socket.Receive(data, data.Length, 0);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+
+                    } while (socket.Available > 0);
+                    if (builder.ToString()!=null)
+                    {
+                        Console.WriteLine("Сервер: " + builder.ToString());
+                    }
+                
+                };
+                //socket.Shutdown(SocketShutdown.Both);
+                //socket.Close();
+
+            }
+            catch (Exception ex)
+            {                                
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+            
+        }
+    }
+}
--- ServerTestApp/Program.cs
+++ ServerTestApp/Program.cs
@@ -5,27 +5,60 @@ using System.Net.Sockets;
 
 namespace ServerTestApp
 {
-    class Program
+    class ServerTest
     {
+
         static int port = 8005;
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             IPEndPoint IpPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"),port);
             Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
             try
             {
                 listenSocket.Bind(IpPoint);
                 listenSocket.Listen(10);
-                Console.WriteLine("Сервер запущен, ожидание подключений...");
+                Console.WriteLine("Сервер запущен, ожидание подключений...");               
+                Socket handler = listenSocket.Accept();
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0;
+                byte[] data = new byte[256];
+                Console.WriteLine("Подключение установлено");
+                while (true)
+                {
+                    do
+                    {
+                        bytes = handler.Receive(data);
+                        builder.Append(Encoding.Unicode.GetString(data, 0,bytes));
+                    } while (handler.Available>0);
 
+                    if (builder.ToString()=="0")
+                    {
+                        handler.Shutdown(SocketShutdown.Both);//остановка как отправки, так и получения данных сокетом
+                        handler.Close();
+                        Console.WriteLine("Удаленный хост принудительно разорвал существующее подключение.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine(DateTime.Now.ToShortTimeString() + ":" + builder.ToString());
+                        builder.Clear();
+                        string message = "Сообщение доставлено";
+                        data = Encoding.Unicode.GetBytes(message);
+                        handler.Send(data);
+                    }
+                    
+                }
+                
             }
             catch (Exception ex)
             {
 
                 Console.WriteLine(ex.Message);
             }
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Hello World!");
+            
+            
         }
     }
 }
